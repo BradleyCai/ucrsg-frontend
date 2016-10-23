6c5af5f691caaa03f3ae2e17c55da3d435f79151
Andrew <iamandrew91791@gmail.com>
2016-10-23 01:24:20 -0700

    Issue #1 - Fix course adding: edge cases (<1 and >4) 	- Duplicate courses not allowed 	- Take the "you haven't added any courses yet" out when a course 	  has been added, puts back Issue #9 - Add function to remove course elements at index and button for all elements

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
+// validate - check with simple regex and that the course hasn't been added yet
 var validate = function (text) {
+	return validateRE(text) && noDups(text);
+};
+var validateRE = function (text) {
 	return text.match(/^\s*([A-Za-z]+)\s+([A-Za-z0-9]+)\s*$/g);
 };
+var noDups = function (text) {
+	var courseIdentifier = toJSON(text);
+	var id = courseIdentifier.id;
+	var subject = courseIdentifier.subject;
+	
+	for (var i = 0; i < courses.courses.length; i++) {
+		if (subject == courses.courses[i]["subject"]
+		&& id == courses.courses[i]["id"]) {
+			return false;
+		}
+	}
+	return true;
+};
 
-// Converts text from "subject id" to JSON
+
+// toJSON - Converts text from "subject id" to JSON
 var toJSON = function (text) {
 	var subject = /^\s*([A-Za-z]+)\s+([A-Za-z0-9]+)\s*$/g.exec(text)[1];
 	var id = /^\s*([A-Za-z]+)\s+([A-Za-z0-9]+)\s*$/g.exec(text)[2];
@@ -11,34 +29,75 @@ var toJSON = function (text) {
 };
 
 
+// hide/showNoClassText - Toggle default text for course input box
+var hideNoClassText = function() {
+	document.getElementById('empty-list').hidden='true';
+};
+var showNoClassText = function() {
+	document.getElementById('empty-list').removeAttribute('hidden');
+};
+
+
+// rmCourseAt - Remove course item at index
+var rmCourseAt = function(index) {
+	// Make sure index is within bounds
+	if (index >= courses.courses.length || index < 0) return false;
+	
+	// Remove from global array
+	courses.courses.splice(index, 1);
+	
+	// Remove from display <==> refresh display entirely
+	refreshCourses();
+	
+	// Display the noClassText if 0 classes added
+	if (courses.courses.length < 1) {
+		showNoClassText();
+	}
+	
+	return true;
+};
+
+
+// refreshCourses - refresh display of courses
+var refreshCourses = function() {
+	var subject, id;
+	$("#input-list")[0].innerHTML = $("#empty-list")[0].outerHTML;
+	for (var i = 0; i < courses.courses.length; i++)
+	{
+		subject = courses.courses[i]["subject"];
+		id = courses.courses[i]["id"];
+		// Add to display
+		$("#input-list").append('<li>' + subject.toUpperCase() + " " + id.toUpperCase() + '</li>');
+	}
+};
+
+var courses = {
+	"courses": []
+};
 
 $(document).ready(function() {
-	var courses = {
-        "courses": []
-    };
 
 	// Add course, input and validate first
 	$("#submit-course").click(function() {
-		var text = $("#course-input").val();
-        var courseIdentifier = toJSON(text);
-        var id = courseIdentifier.id;
-        var subject = courseIdentifier.subject;
 		
-        console.log(courseIdentifier.id);
+		var text = $("#course-input").val();
 
 		// Validate and input text
-		if (subject !== null && id !== null) {
-            var empty_list = $("#empty-list");
-
-            if (empty_list === undefined) {
-                empty_list.remove();
-            }
-    			$("#input-list").append('<li>' + subject.toUpperCase() + " " + id.toUpperCase() + '</li>');
-
-			courses.courses.push(courseIdentifier);
+		if (validate(text)) {
+			
+			// Add to global courses list
+			courses.courses.push(toJSON(text));
+			
+			// Refresh display
+			refreshCourses();
+			
+			// Hide the default text in input list box
+			hideNoClassText();
+			
 		}
 		else {
 			// TODO: needs to be some other form of notification?
+			// TODO: Issue #2
 			alert("Input text invalid.");
 		}
 		
@@ -51,9 +110,22 @@ $(document).ready(function() {
             $('#course-input').val("");
         }
     });
+	
+	
+	// Remove All Courses Button
+	$("#remove-all").click(function() {
+		console.log("remove-all function entered");
+		courses.courses = [];
+		refreshCourses();
+	});
+	
 
     // Have the #generate button send the data to the server
 	$("#generate").click(function() {
+		// Error bounds checking
+		if (courses.courses.length < 1 || courses.courses.length > 4) return;
+		// TODO: Issue #2
+		
 		var sendString = JSON.stringify(courses);
 
 		alert("Sending to server: " + sendString);
