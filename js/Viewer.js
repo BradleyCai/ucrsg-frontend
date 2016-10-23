8e7421dc6e3a2e12a93b78a7063286cdeb2573fb
BradleyCai <bradcai24@gmail.com>
2016-10-22 18:35:10 -0700

    Added more organization to the js

---
+++ js/Viewer.js
@@ -0,0 +1,27 @@
+/** Views are snippets of HTML. For example the first screen with the input text
+  * box and the buttons and card are all part of the "InputView".
+  *
+  * This "Viewer" is in charge of maintaining and dealing with the views. It holds
+  * a view object and a couple functions to manipulate it's data
+  *
+  */
+var Viewer = {
+    // Will add a view
+    addView: function() {
+
+    },
+
+    // Will set a view to display
+    setView: function() {
+
+    },
+
+    // Will remove a certain view. Default is to have it fade away
+    removeView: function() {
+
+    },
+
+    // Object of views. Contains the jQuery HTML objects of each view. Views
+    // can also be stored in an array to make iteration easier
+    views: {}
+};
